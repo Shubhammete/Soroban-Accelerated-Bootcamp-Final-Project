ec214249e8218270b90a165fce0450fdfee69ad5
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2024-04-06 12:18:22 +0530

    Rust data types

---
+++ Rust Bootcamp/hello-rust/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    println!("Hello, world!");
+
+    // data types
+
+    // int 
+    // i8 i16 i32 i64 u8 u16 u32 u64 
+    let num : i32 = 42;
+
+    // bool
+    let isTrue : bool = true;
+
+    // char
+    let c: char = 'a';
+
+    // float
+    //f32 f64
+    let float_no:f32 = 23.4;
+
+    // tuple
+    let new_tuple:(i32,char,bool) = (33,'a',false);
+
+    //array
+    let new_Arr : [i32,3] = [2,3,4];
+}
